5f5e7f3352212d861052e722c9c211184f5ab80a
hassan1876 <95700641+hassan1876@users.noreply.github.com>
2025-11-19 11:49:00 +0200

    feat:add integration in ActivityProcessingService

---
+++ ActivityProcessingService/index.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+import ProcessActivityLog from './src/application/services/ProcessActivityLogService.js'
+import KafkaConsumerAdapter from './infrastructure/kafka/KafkaConsumerAdapter.js'
+import MongoActivityLogRepository from './src/infrastructure/database/MongoActivityLogRepository.js'
+
+dotenv.config()
+const mongoActivityLogRepo = new MongoActivityLogRepository({
+    connectionString:process.env.CONNECTION_STRING
+})
+const processActivityLogService = new ProcessActivityLog({
+    repo:mongoActivityLogRepo
+})
+const messageConsumer = new KafkaConsumerAdapter({
+    brokerUrl:process.env.KAFKA_BROKER,
+    topic:process.env.KAFKA_TOPIC_ID,
+    processLogService:processActivityLogService
+})
+
+;(async () => {
+    try{
+        await messageConsumer.connect()
+        await messageConsumer.receiveActivityLog()
+    }catch(e){
+        throw e
+    }
+})()
+
+
